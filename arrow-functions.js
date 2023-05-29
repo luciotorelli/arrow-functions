64bbc97227b71df34c285e4b7d1febcd26ef11a1
luciotorelli <luciotorelli@hotmail.com>
2023-05-29 12:35:42 +0100

    Finish arrow functions exercise

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,43 @@
  */
 
 // Vanilla JavaScript Function
+function addTwoNumbers1(a, b) {
+    // Code Block
+    return a + b;
+}
+
+let sum1 = addTwoNumbers1(3, 5);
+console.log(sum1);
 
 // Arrow Function With Parameters
 
+const addTwoNumbers2 = (a, b) => {
+    // Code Block
+    return a + b;
+}
+
+let sum2 = addTwoNumbers2(3, 5);
+console.log(sum2);
+
 // Single Line Arrow Function With Parameters
 
+const addTwoNumbers3 = (a, b) => a + b;
+let sum3 = addTwoNumbers2(6, 4);
+console.log(sum3);
+
 // Implicit Returns
+const saySomething = message => console.log(message);
+saySomething('Hello there!');
+
+const sayHellow = () => console.log('hello');
+sayHellow();
 
 // Returning Multiple Lines
+
+const returnMultipleLines = () => (
+    `<p>
+        This is a multiline string!
+    </p>
+    `
+)
+console.log(returnMultipleLines());
